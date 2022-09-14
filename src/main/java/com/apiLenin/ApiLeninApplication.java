6f6e1e6fe9184ec667fb0c583b2ac93e4a6bff35
Lenin Josue Moreno <90065038+LeninJosueMoreno@users.noreply.github.com>
2022-09-14 11:41:32 -0500

    Api

---
+++ src/main/java/com/apiLenin/ApiLeninApplication.java
@@ -0,0 +1,13 @@
+package com.apiLenin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLeninApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLeninApplication.class, args);
+	}
+
+}
